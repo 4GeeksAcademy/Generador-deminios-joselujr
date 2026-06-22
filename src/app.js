9776ca1c2777d7d390be0626e31cac98a56aa6f1
Jose Rodriguez <masullo.joseluis@gmail.com>
2026-06-22 19:11:11 +0000

    cambio por forEach

--- src/app.js
+++ src/app.js
@@ -14,11 +14,20 @@ const marca = ["toyota/", "honda/", "lexus/"];
 const color = ["negro/", "rojo/", "plata/"];
 const motor = ["gasolina", "diesel", "electrico"]
 const fin = [".com", ".net", ".lol"]
-for (let i = 0; i < marca.length; i++) {
-  for (let j = 0; j < color.length; j++) {
-    for (let k = 0; k < color.length; k++) {
-      console.log(marca[i] + color[j] + motor[k] + fin[Math.floor(Math.random() * fin.length)]);
-    }
 
-  }
-}
+marca.forEach(ma =>
+  color.forEach(co =>
+    motor.forEach(mo =>
+      console.log(`${ma}${co}${mo}${fin[Math.floor(Math.random() * fin.length)]}`)
+    )
+  )
+);
+
+
+//for (let i = 0; i < marca.length; i++) {
+//for (let j = 0; j < color.length; j++) {
+// for (let k = 0; k < color.length; k++) {
+//console.log(marca[i] + color[j] + motor[k] + fin[Math.floor(Math.random() * fin.length)]);
+//}
+//}
+//}  
